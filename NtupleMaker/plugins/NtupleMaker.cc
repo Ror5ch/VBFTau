88f7ff717a724be4bb9f5db0ef686bb1dca7616c
Braden <braden.allmond@gmail.com>
2021-04-03 05:05:05 +0200

    Added trigger to Ntuplizer and comments

--- NtupleMaker/plugins/NtupleMaker.cc
+++ NtupleMaker/plugins/NtupleMaker.cc
@@ -36,6 +36,11 @@
 #include "CommonTools/UtilAlgos/interface/TFileService.h" 
 
 #include "TTree.h"
+
+//added from other file for trigger names (?)
+#include "FWCore/Common/interface/TriggerNames.h"
+#include "DataFormats/HLTReco/interface/TriggerEvent.h" //might be old/deprecated, other script written in 2017
+#include "DataFormats/HLTReco/interface/TriggerObject.h"
 //
 // class declaration
 //
@@ -62,6 +67,7 @@ class NtupleMaker : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
       virtual void endJob() override;
 
       // ----------member data ---------------------------
+      // tracks
       edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
       TTree* tree_;
 
@@ -69,6 +75,12 @@ class NtupleMaker : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 
       int ntracks_;
       int track_charge_[MAX_SIZE];
+      // hlt
+      //edm::InputTag triggerResultTag_;
+      edm::EDGetTokenT<edm::TriggerResults>   triggerResultToken_; // might not need the second edm on this line
+      //edm::InputTag triggerSummTag_;
+      edm::EDGetTokenT<trigger::TriggerEvent> triggerEventToken_;
+      bool triggered_;
 };
 
 //
@@ -83,8 +95,13 @@ class NtupleMaker : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 // constructors and destructor
 //
 NtupleMaker::NtupleMaker(const edm::ParameterSet& iConfig):
-    tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks")))
-
+    tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks"))), //this comma may not need to be here?
+    triggerResultToken_(consumes<TriggerResults>(iConfig.getUntrackedParameter<edm::InputTag>("triggerResults"))), //consumes is a cpp memory thing
+    triggerEventToken_(consumes<trigger::TriggerEvent>(iConfig.getUntrackedParameter<edm::InputTag>("triggerEvent")))
+//"tracks" as a string isn't defined previously so I think it refers to the tracks variable defined in the config file,
+//accordingly, we define a trigger result variable (?)
+//looks like the case when comparing to other code but remember to ask Kyungwook
+//what the other script does in two lines Kyungwook does in one, that's why we're all tokens and no tags here  
 {
     //now do what ever initialization is needed
 }
@@ -106,8 +123,9 @@ NtupleMaker::~NtupleMaker()
 // ------------ method called for each event  ------------
 void NtupleMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+   using namespace edm; //isn't this line redundant?
 
+	// tracks
     edm::Handle<TrackCollection> tracks;
     iEvent.getByToken(tracksToken_, tracks);
 
@@ -118,7 +136,28 @@ void NtupleMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
     {
       track_charge_[i++] = track.charge();
     }
+	// hlt
+    edm::Handle<TriggerResults> triggerResults;
+    iEvent.getByToken(triggerResultToken_, triggerResults); //adding trigger event may be necessary? edm::Handle<trigger::TriggerEvent> triggerEvent;
+    edm::Handle<trigger::TriggerEvent> triggerEvent; //instantiation
+    iEvent.getByToken(triggerEventToken_, triggerEvent); //assignment
+    edm::TriggerNames triggerNames_ = iEvent.triggerNames(*triggerResults);
 
+    // copying the meat of the function from the other file
+    triggered_ = false;
+    for (unsigned int itrig = 0; itrig < triggerNames.size(); ++itrig)
+    {
+     LogDebug ("triggers") << triggerNames.triggerName(itrig) ; //is this native cpp? doesn't seem to be related to edm... may need to comment out
+     if (triggerResults->accept(itrig))
+     {
+      std::string pathName = triggerNames.triggerName(itrig); //triggerName must be a method of triggerNames ?
+      if ( pathName.find("HLT_VBF") != std::string::npos )
+      {
+       iEvent_.hlt.triggers.push_back(pathName);
+       triggered_ = true;
+      }
+     }
+    }
     tree_->Fill();
 }
 
@@ -130,6 +169,7 @@ void NtupleMaker::beginJob()
     tree_ = fs->make<TTree>("vbf", "vbf");
     tree_->Branch("ntracks_", &ntracks_);
     tree_->Branch("track_charge_", track_charge_, "track_charge_[ntracks_]/I");
+    tree_->Branch("HLT_triggered", triggered_, "triggered/B");
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
@@ -141,15 +181,15 @@ void NtupleMaker::endJob()
 void NtupleMaker::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
     //The following says we do not know what parameters are allowed so do no validation
     // Please change this to state exactly what you do use, even if it is no parameters
-    edm::ParameterSetDescription desc;
-    desc.setUnknown();
-    descriptions.addDefault(desc);
+    //edm::ParameterSetDescription desc;
+    //desc.setUnknown();
+    //descriptions.addDefault(desc);
 
     //Specify that only 'tracks' is allowed
     //To use, remove the default given above and uncomment below
-    //ParameterSetDescription desc;
-    //desc.addUntracked<edm::InputTag>("tracks","ctfWithMaterialTracks");
-    //descriptions.addDefault(desc);
+    edm::ParameterSetDescription desc;
+    desc.addUntracked<edm::InputTag>("tracks","ctfWithMaterialTracks");
+    descriptions.addDefault(desc);
 }
 
 //define this as a plug-in
--- NtupleMaker/python/ConfFile_cfg.py
+++ NtupleMaker/python/ConfFile_cfg.py
@@ -9,7 +9,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+        'file:/afs/cern.ch/user/b/ballmond/private/CMSSW_10_1_11_patch1/src/HLTrigger/Configuration/test/addedhltVBF.root'
     )
 )
 

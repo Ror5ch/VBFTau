2afac858477cbb4897c5651843421a7b82a10477
Braden <braden.allmond@gmail.com>
2021-04-14 18:37:42 +0200

    added eta, crashes cmsRun

--- NtupleMaker/plugins/NtupleMaker.cc
+++ NtupleMaker/plugins/NtupleMaker.cc
@@ -19,6 +19,7 @@
 
 // system include files
 #include <memory>
+#include <vector>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -69,17 +70,22 @@ class NtupleMaker : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 
       // ----------member data ---------------------------
       // tracks
-      edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
+      //edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
       TTree* tree_;
 
       static constexpr int MAX_SIZE = 1000;
 
-      int ntracks_;
-      int track_charge_[MAX_SIZE];
+      //int ntracks_;
+      //int track_charge_[MAX_SIZE];
       // hlt
       edm::EDGetTokenT<edm::TriggerResults>   triggerResultToken_;
       edm::EDGetTokenT<trigger::TriggerEvent> triggerEventToken_;
+      std::vector<bool> * triggeredBranch_;
       bool triggered_;
+      float pt_;
+      float eta_;
+      std::vector<float> * ptBranch1_;
+      std::vector<float> * etaBranch1_;
 };
 
 //
@@ -94,7 +100,7 @@ class NtupleMaker : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 // constructors and destructor
 //
 NtupleMaker::NtupleMaker(const edm::ParameterSet& iConfig):
-    tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks"))),
+    //tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks"))),
     triggerResultToken_(consumes<edm::TriggerResults>(iConfig.getUntrackedParameter<edm::InputTag>("triggerResults"))),
     triggerEventToken_(consumes<trigger::TriggerEvent>(iConfig.getUntrackedParameter<edm::InputTag>("triggerEvent")))
 //"tracks" as a string isn't defined previously so I think it refers to the tracks variable defined in the config file,
@@ -122,28 +128,59 @@ NtupleMaker::~NtupleMaker()
 void NtupleMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
    using namespace edm;
-
-	// tracks
-    edm::Handle<TrackCollection> tracks;
-    iEvent.getByToken(tracksToken_, tracks);
-
-    ntracks_ = tracks->size();
-
-    auto i = 0;
-    for (const auto& track : *tracks)
-    {
-      track_charge_[i++] = track.charge();
-    }
 	// hlt
     edm::Handle<TriggerResults> triggerResults;
     iEvent.getByToken(triggerResultToken_, triggerResults);
     edm::Handle<trigger::TriggerEvent> triggerEvent; //instantiation
     iEvent.getByToken(triggerEventToken_, triggerEvent); //assignment
     const edm::TriggerNames triggerNames_ = iEvent.triggerNames(*triggerResults); //from online tutorial
-
-    triggered_ = false;
     std::string pathName="HLT_VBF_DoubleTightChargedIsoPFTauHPS20_Trk1_eta2p1_v1";
     triggered_ = triggerResults->accept(triggerNames_.triggerIndex(pathName));
+    triggeredBranch_->push_back(triggered_);
+
+
+    const trigger::size_type nFilters(triggerEvent->sizeFilters());
+
+    std::string firstTag = "hltMatchedVBFTwoPFJets2CrossCleanedFromDoubleTightChargedIsoPFTauHPS20::MYHLT";
+    for (trigger::size_type iFilter=0; iFilter!=nFilters; ++iFilter){
+        std::string filterTag = triggerEvent->filterTag(iFilter).encode();
+        trigger::Keys objectKeys = triggerEvent->filterKeys(iFilter);
+	const trigger::TriggerObjectCollection& triggerObjects(triggerEvent->getObjects());
+	for(trigger::size_type iKey=0; iKey < objectKeys.size(); ++iKey){
+	    trigger::size_type objKey = objectKeys.at(iKey);
+	    const trigger::TriggerObject& triggerObj(triggerObjects[objKey]);
+	    pt_ = triggerObj.pt();
+	    eta_ = triggerObj.eta();
+	    if (filterTag == firstTag && pt_ > 0){
+		ptBranch1_->push_back(pt_);
+		std::cout << std::endl << "pt: " <<  pt_ << std::endl;
+		//etaBranch1_->push_back(eta_);
+	    }
+	    else{
+		ptBranch1_->push_back(-9999.);
+		//etaBranch1_->push_back(-9999.);
+	    }
+	}
+    }
+
+    //std::string filter1("hltHpsDoublePFTau20TrackTightChargedIsoAgainstMuon");
+//implementation from https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideHLTAnalysis
+//    trigger::size_type filterIndex = triggerEvent->filterIndex(edm::InputTag(filterName,"","MYHLT")); 
+//    if(filterIndex < triggerEvent->sizeFilters()){ 
+//        const trigger::Keys& triggerKeys = triggerEvent->filterKeys(filterIndex); 
+//        const trigger::TriggerObjectCollection & trigObjColl(triggerEvent->getObjects());
+        //now loop on the trigger objects passing filter
+//        for(trigger::Keys::const_iterator keyIt=triggerKeys.begin(); keyIt!=triggerKeys.end(); ++keyIt){ 
+//            const trigger::TriggerObject& obj = trigObjColl[*keyIt];
+            //do what you want with the trigger objects, you have
+            //eta,phi,pt,mass,p,px,py,pz,et,energy accessors
+//            pt_ = obj.pt();
+//	    std::cout << pt_ << std::endl;
+//	    ptBranch_->push_back(pt_);
+//        }
+                                          
+//    }//end filter size check
+
     tree_->Fill();
 }
 
@@ -153,9 +190,10 @@ void NtupleMaker::beginJob()
 {
     edm::Service<TFileService> fs;
     tree_ = fs->make<TTree>("vbf", "vbf");
-    tree_->Branch("ntracks_", &ntracks_);
-    tree_->Branch("track_charge_", track_charge_, "track_charge_[ntracks_]/I");
-    tree_->Branch("HLT_triggered", triggered_, "triggered/B");
+    //tree_->Branch("ntracks_", &ntracks_);
+    //tree_->Branch("track_charge_", track_charge_, "track_charge_[ntracks_]/I");
+    tree_->Branch("triggeredBranch_", &triggeredBranch_);
+    tree_->Branch("ptBranch1_", &ptBranch1_);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------

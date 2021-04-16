9a5df6b320421539d06cc0813083a156e175efed
Braden <braden.allmond@gmail.com>
2021-04-16 06:57:08 +0200

    added branch filling to function in Ntuplizer_triggers.cc

--- NtupleMaker/plugins/NtupleMaker_trigger.cc
+++ NtupleMaker/plugins/NtupleMaker_trigger.cc
@@ -46,7 +46,9 @@ void NtupleMaker::branchesTriggers(TTree* tree){
 
 void NtupleMaker::fillTriggers(const edm::Event& iEvent){
 
-    //using namespace edm; 
+    using namespace edm;
+
+    // clearing vectors at the start of every event 
     passTrigBranch.clear();
 
     hltHpsDoublePFTau_pt.clear();
@@ -59,6 +61,7 @@ void NtupleMaker::fillTriggers(const edm::Event& iEvent){
     hltMatchedVBFTwo_eta.clear();
     hltMatchedVBFTwo_phi.clear();
 
+    // getting trigger results and saving to passTrigBranch
     edm::Handle<edm::TriggerResults> triggerResults;
     iEvent.getByToken(triggerResultToken_, triggerResults);
     edm::Handle<trigger::TriggerEvent> triggerEvent;
@@ -66,7 +69,57 @@ void NtupleMaker::fillTriggers(const edm::Event& iEvent){
     const edm::TriggerNames triggerNames_ = iEvent.triggerNames(*triggerResults);
     std::string pathName="HLT_VBF_DoubleTightChargedIsoPFTauHPS20_Trk1_eta2p1_v1";
     passTrig_ = triggerResults->accept(triggerNames_.triggerIndex(pathName));
-    std::cout << "passTrig: " << passTrig_ << std::endl;
     passTrigBranch.push_back(passTrig_);
 
-}
+    // filling branches with triggerObjs information
+    const trigger::size_type nFilters(triggerEvent->sizeFilters());
+    std::string hltHpsDoublePFTau_Tag = "hltHpsDoublePFTau20TrackTightChargedIsoAgainstMuon::MYHLT";
+    std::string hltMatchedVBFOne_Tag = "hltMatchedVBFOnePFJets2CrossCleanedFromDoubleTightChargedIsoPFTauHPS20::MYHLT";
+    std::string hltMatchedVBFTwo_Tag = "hltMatchedVBFTwoPFJets2CrossCleanedFromDoubleTightChargedIsoPFTauHPS20::MYHLT";
+
+    for(trigger::size_type iFilter=0; iFilter!=nFilters; ++iFilter){
+	std::string filterTag = triggerEvent->filterTag(iFilter).encode();
+	trigger::Keys objectKeys = triggerEvent->filterKeys(iFilter);
+	const trigger::TriggerObjectCollection& triggerObjects(triggerEvent->getObjects());
+	for(trigger::size_type iKey=0; iKey < objectKeys.size(); ++iKey){
+	    trigger::size_type objKey = objectKeys.at(iKey);
+	    const trigger::TriggerObject& triggerObj(triggerObjects[objKey]);
+	    pt_ = triggerObj.pt();
+	    eta_ = triggerObj.eta();
+	    phi_ = triggerObj.phi();
+	// fill hltHpsDoublePFTau branches if match
+	    if (filterTag == hltHpsDoublePFTau_Tag && pt_>0){
+		hltHpsDoublePFTau_pt.push_back(pt_); 
+		hltHpsDoublePFTau_eta.push_back(eta_);
+		hltHpsDoublePFTau_phi.push_back(phi_);
+	    }
+	    else{
+		hltHpsDoublePFTau_pt.push_back(-99);
+		hltHpsDoublePFTau_eta.push_back(-99);
+		hltHpsDoublePFTau_phi.push_back(-99);
+	    }
+	// fill hltMatchedVBFOne branches if match
+            if (filterTag == hltMatchedVBFOne_Tag && pt_>0){
+                hltMatchedVBFOne_pt.push_back(pt_);
+                hltMatchedVBFOne_eta.push_back(eta_);
+                hltMatchedVBFOne_phi.push_back(phi_);
+            }
+            else{
+                hltMatchedVBFOne_pt.push_back(-99);
+                hltMatchedVBFOne_eta.push_back(-99);
+                hltMatchedVBFOne_phi.push_back(-99);
+            }
+	// fill hltMatchedVBFTwo branches if match
+            if (filterTag == hltMatchedVBFTwo_Tag && pt_>0){
+                hltMatchedVBFTwo_pt.push_back(pt_);
+                hltMatchedVBFTwo_eta.push_back(eta_);
+                hltMatchedVBFTwo_phi.push_back(phi_);
+            }
+            else{
+                hltMatchedVBFTwo_pt.push_back(-99);
+                hltMatchedVBFTwo_eta.push_back(-99);
+                hltMatchedVBFTwo_phi.push_back(-99);
+            }
+	} // end loop over trigger object keys
+    } // end loop over nfilters
+} // end function

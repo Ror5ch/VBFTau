aa7dfd599c7763438501656c28155063b719732e
Braden Allmond <ballmond@login06.hep.wisc.edu>
2021-05-10 10:21:53 -0500

    updated trigger_selection.cc to be more like Doyeong's SMHTT analyzer code

--- NtupleMaker/python/trigger_selection.cc
+++ NtupleMaker/python/trigger_selection.cc
@@ -19,9 +19,10 @@
 #include "TLorentzVector.h"
 #include "TRandom3.h"
 
+#include "trigger_tree.h"
 
 int main(int argc, char** argv)	{
-    //myMap1 = new map<std::string, TH1F*>(); //?
+    //myMap1 = new map<std::string, TH1F*>(); // i think this is for generalized naming/name fixing of different files
     //////////////////////////////////////
     // Input Arguments			//
     // 1) Input Path			//
@@ -33,20 +34,32 @@ int main(int argc, char** argv)	{
     std::string outName = out;
     TFile *fIn = TFile::Open(inName.c_str());
     //fIn->SetDirectory(0);
+    TTree* treePtr = (TTree*) fIn->Get("demo/vbf");
+    trigger_tree* inTree = new trigger_tree (treePtr);
 
     // Open input file and make new output file
-    TTree *inTree = (TTree*)fIn->Get("demo/vbf");
+    //TTree *inTree = (TTree*)fIn->Get("demo/vbf");
     TTree *outTree = new TTree("outTree", "outTree");
-    outTree->SetDirectory(0);
+    //outTree->SetDirectory(0);
     //here's where you'd make a header file for that input file i think...
-    int VBFTwoTriggerPt;
-    outTree->Branch("VBFTwoTriggerPt", &VBFTwoTriggerPt);
+    int VBFTwoTriggerEta;
+    outTree->Branch("VBFTwoTriggerEta", &VBFTwoTriggerEta);
 
     // Event Loop
     for (int iEntry = 0; iEntry < inTree->GetEntries(); iEntry++) {
 	inTree->GetEntry(iEntry);
 	if (iEntry % 1000 == 0) { std::cout << std::to_string(iEntry) << std::endl;}
+        //inTree->GetBranch("hltMatchedVBFTwo_eta");
+	VBFTwoTriggerEta = inTree->hltMatchedVBFTwo_eta;
+	if (fabs(VBFTwoTriggerEta) > 2.1) continue;
+	outTree->Fill();
+
     } // end event loop
 
+    std::string outputFileName = outName;
+    TFile *fOut = TFile::Open(outputFileName.c_str(),"RECREATE");
+    fOut->cd();
+    outTree->Write();
+    fOut->Close();
     return 0;
 }

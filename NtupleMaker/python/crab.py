77ff21f13f088ab6ab55ff58e0b121d25724dad9
Braden Allmond <braden.allmond@gmail.com>
2021-05-26 22:57:11 -0500

    removing deprecated files from repo

--- NtupleMaker/python/crab.py
+++ NtupleMaker/python/crab.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     from CRABAPI.RawCommand import crabCommand
 
-    config.General.requestName = 'VBFHToTauTau_HLT_and_AOD_05252021_Reconfig'
+    config.General.requestName = 'VBFHToTauTau_HLT_and_AOD_05252021_Reconfig_Retry'
     config.Data.inputDataset = '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISpring18MiniAOD-NZSPU28to70_100X_upgrade2018_realistic_v10-v1/MINIAODSIM'
     config.Data.secondaryInputDataset = '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISpring18DR-NZSPU28to70_100X_upgrade2018_realistic_v10-v1/GEN-SIM-RAW'
     #config.Data.inputDataset = '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISpring18DR-NZSPU28to70_100X_upgrade2018_realistic_v10-v1/GEN-SIM-RAW'
